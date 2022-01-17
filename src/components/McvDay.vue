ff3b51e07008b9bf5d165e2a17842383877115fc
uzunvictor <https://github.com/uzunvictor>
2022-01-17 21:17:16 +0200

    first steps of creating modal window

--- src/components/McvDay.vue
+++ src/components/McvDay.vue
@@ -1,14 +1,39 @@
 <template>
   <div class="container">
-    <h1>{{ weekDay }}, {{ day }} {{ month }} {{ year }}</h1>
+    <div class="tasks-header">
+      <h1 class="tasks-date">
+        {{ weekDay }}, {{ day }} {{ month }} {{ year }}
+      </h1>
+      <button class="create-new-task" @click="createNewTask">New Task</button>
+    </div>
+
+    <mcv-tasks />
+    <transition name="fade" appear>
+      <div class="modal-overlay" v-if="isTask" @click="isTask = false"></div>
+    </transition>
+    <transition name="slide" appear>
+    <div class="modal" v-if="isTask">
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi aliquid minus obcaecati deserunt, voluptas totam eveniet laborum harum doloribus facilis?</p>
+      <!-- Alt + z -->
+      <button class="button" @click="isTask = false">Hide</button>
+    </div>
+    </transition>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
+import McvTasks from "@/components/McvTasks";
 
 export default {
   name: "McvDay",
+
+  data() {
+    return {
+      isTask: false,
+    };
+  },
 
   computed: {
     ...mapState({
@@ -24,6 +49,16 @@ export default {
 
   mounted() {
     console.log("weekday", this.weekDay);
+  },
+
+  components: {
+    McvTasks,
+  },
+
+  methods: {
+    createNewTask() {
+      this.isTask = true;
+    },
   },
 };
 </script>
---
+++ src/components/McvTasks.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="tasks">Mcv Tasks</div>
+</template>
+
+<script>
+export default {
+  name: "McvTasks",
+};
+</script>
